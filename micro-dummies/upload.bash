f450c3fcaae569f7f0dd6b0e10f2d9fc35a509cd
Camilo Nossa <calejandro_noss@outlook.com>
2025-11-01 20:42:11 -0500

    refactor: update service names and references in upload script, docker-compose, and .dockerignore for consistency and alignment with naming conventions

--- micro-dummies/upload.bash
+++ micro-dummies/upload.bash
@@ -6,5 +6,5 @@ IMAGE_TAG="1.5.0"
 aws ecr-public get-login-password --region $AWS_REGION \
 | docker login --username AWS --password-stdin public.ecr.aws
 
-docker tag dummy/venta-ms:$IMAGE_TAG public.ecr.aws/$ALIAS/$REPO:$IMAGE_TAG
+docker tag dummy/sales:$IMAGE_TAG public.ecr.aws/$ALIAS/$REPO:$IMAGE_TAG
 docker push public.ecr.aws/$ALIAS/$REPO:$IMAGE_TAG
